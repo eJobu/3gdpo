47cd23a01a80d4c9e352a61828d950f45721960f
samueltuyizere <samueltuyizere0@gmail.com>
2022-08-23 19:36:00 +0200

    added the main class to connect with the API
    Signed-off-by: samueltuyizere <samueltuyizere0@gmail.com>

---
+++ lib/main/index.ts
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import { httpMethod } from 'types'
+
+export class Api {
+  public async connect (
+    method: httpMethod,
+    body?: any
+  ): Promise<any> {
+    const url = 'https://secure.3gdirectpay.com/API/V6'
+    const headers = {
+      'Content-Type': 'application/xml'
+    }
+    try {
+      const request = await axios({
+        url,
+        method,
+        headers,
+        data: JSON.stringify(body)
+      })
+      if (request.status.toString().startsWith('4') || request.status.toString().startsWith('5')) {
+        console.error(`Error returned by the API: ${request.statusText}`)
+        throw new Error(request.statusText)
+      }
+      return request.data
+    } catch (error) {
+      console.log(`There was an error connecting to the API: ${error.message}`)
+      throw new Error(error.message)
+    }
+  }
+}
+
+export const dpoApi = new Api()
