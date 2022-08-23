41605ca575b2a017e34889e70a1288806b87a8cd
samueltuyizere <samueltuyizere0@gmail.com>
2022-08-23 19:36:39 +0200

    implemented the function to create a simple payment
    Signed-off-by: samueltuyizere <samueltuyizere0@gmail.com>

---
+++ lib/redirectedPayments/index.ts
@@ -0,0 +1,35 @@
+import { dpoApi } from 'main'
+import { httpMethod } from 'types'
+
+export async function createAsimplePayment (
+  companyToken: string,
+  amount: number,
+  currency: string,
+  companyRef: string,
+  redirectUrl: string,
+  reference: string,
+  description: string
+): Promise<void> {
+  const data = `
+  <?xml version="1.0" encoding="utf-8"?>
+<API3G>
+    <CompanyToken>${companyToken}</CompanyToken>
+    <Request>createToken</Request>
+    <Transaction>
+        <PaymentAmount>${amount}</PaymentAmount>
+        <PaymentCurrency>${currency}</PaymentCurrency>
+        <CompanyRef>${companyRef}</CompanyRef>
+        <RedirectURL>${redirectUrl}</RedirectURL>
+        <CompanyRefUnique>${reference}</CompanyRefUnique>
+        <PTL>5</PTL>
+    </Transaction>
+    <Services>
+        <Service>
+            <ServiceDescription>${description}</ServiceDescription>
+            <ServiceDate>${new Date().toUTCString()}</ServiceDate>
+        </Service>
+    </Services>
+</API3G>
+  `
+  return await dpoApi.connect(httpMethod.POST, data)
+}
