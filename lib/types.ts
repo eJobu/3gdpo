e3fe09b2860b1bcfc64268fbd878d72da4374e3b
samueltuyizere <samueltuyizere0@gmail.com>
2022-08-23 19:35:26 +0200

    added initial enums
    Signed-off-by: samueltuyizere <samueltuyizere0@gmail.com>

---
+++ lib/types.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+export enum httpMethod {
+  POST = 'POST',
+  GET = 'GET',
+  PUT = 'PUT',
+  DELETE = 'DELETE'
+}
+
+export enum Endpoints {
+  createToken = 'API/v6/'
+}
